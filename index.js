ad7c9d9ba038e17e4e3020b675b3be89079a3ae4
Kosta Klevensky <kosta@codefresh.io>
2016-06-21 17:53:09 +0300

    fixed html

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ app.use(bodyParser.json());
 app.use('/', router);
 
 function getHtContent() {
-  return new Date().toISOString();
+  return new Date().toISOString().replace(/T|Z/g, " ");
 }
 
 router.get('/', function(req, res) {
   
   var htHeader = '<HTML>' + 
                    '<HEAD><STYLE> ' +
-                       'body{ background-color: lightblue; padding-top: 150px; text-align: center; font: 30px monospace; }' +
+                       'body{ background-color: lightblue; padding-top: 150px; text-align: center; font: 50px monospace; }' +
                    '</STYLE></HEAD>' + 
                    '<BODY>';                                         
   var htContent = getHtContent(); 
@@ -23,7 +23,7 @@ router.get('/', function(req, res) {
   
   res.send( htHeader + 
                 '<div>' + htContent +  ' on server</div>' +
-                '<div><script>document.write(new Date().toISOString() + " on client");</script></div>' +
+                '<div><script>document.write(new Date().toISOString().replace(/T|Z/g, " ") + " on client");</script></div>' +
             htFooter).end();
 });
 
