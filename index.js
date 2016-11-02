5175656b08b473f0302afdda731b9875653fd1b5
Kosta Klevensky <kosta@codefresh.io>
2016-11-02 17:00:00 +0200

    deploy test commit 2

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ router.get('/', function(req, res) {
   var htFooter = '</BODY></HTML>';
   
   res.send( htHeader +
-                '<div>Hostname WWWW ' + os.hostname() +  '</div>' +
+                '<div>Hostname SWDER ' + os.hostname() +  '</div>' +
                 '<div>Time ' + htContent +  ' on server</div>' +
                 '<div><script>document.write("Time " + new Date().toISOString().replace(/T|Z/g, " ") + " on client");</script></div>' +
             htFooter).end();
